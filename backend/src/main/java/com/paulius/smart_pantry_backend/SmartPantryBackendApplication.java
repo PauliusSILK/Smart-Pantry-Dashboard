3ad6940870c8bf28407ee24d5e82137c20d4af3d
PauliusSILK <726259@student.inholland.nl>
2026-06-12 13:35:36 +0200

    feat: initialize frontend with React, Vite, and TypeScript setup
    - Added package.json for project dependencies and scripts
    - Created favicon.svg and icons.svg for application branding
    - Implemented App.css for styling components
    - Developed App.tsx as the main application component
    - Set up index.css for global styles and theming
    - Configured main.tsx for rendering the application
    - Established TypeScript configuration files for app and node
    - Created vite.config.ts for Vite configuration

---
+++ backend/src/main/java/com/paulius/smart_pantry_backend/SmartPantryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.paulius.smart_pantry_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPantryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPantryBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/paulius/smart_pantry_backend/SmartPantryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulius.smart_pantry_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPantryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
